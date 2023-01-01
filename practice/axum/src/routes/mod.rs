3321c50f799a8e43d338db19b9703760f63b1bd1
張継 <choutsugi@gmail.com>
2023-01-01 17:50:43 +0800

    response with json

--- practice/axum/src/routes/mod.rs
+++ practice/axum/src/routes/mod.rs
@@ -1,4 +1,5 @@
 mod always_errors;
+mod get_json;
 mod hello_world;
 mod middleware_message;
 mod mirror_body_json;
@@ -19,6 +20,7 @@ use axum::{
     routing::{get, post},
     Extension, Router,
 };
+use get_json::get_json;
 use hello_world::hello_world;
 use middleware_message::middleware_message;
 use mirror_body_json::mirror_body_json;
@@ -64,6 +66,7 @@ pub fn create_routes() -> Router<Body> {
         .route("/middleware_message", get(middleware_message))
         .route("/always_errors", get(always_errors))
         .route("/returns_201", get(returns_201))
+        .route("/get_json", get(get_json))
         .layer(cors)
         .layer(Extension(share_data))
 }
