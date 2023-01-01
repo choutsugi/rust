31b3ccfdfecdbf3bb1387e1d03bd7a581895a218
張継 <choutsugi@gmail.com>
2023-01-01 13:48:35 +0800

    standard header

--- practice/axum/src/routes/header_user_agent.rs
+++
@@ -1,5 +0,0 @@
-use axum::{headers::UserAgent, TypedHeader};
-
-pub async fn header_user_agent(TypedHeader(agent): TypedHeader<UserAgent>) -> String {
-    agent.to_string()
-}
--- practice/axum/src/routes/mod.rs
+++ practice/axum/src/routes/mod.rs
@@ -1,20 +1,20 @@
-mod header_user_agent;
 mod mirror_body_json;
 mod mirror_body_string;
 mod path_variables;
 mod query_params;
 mod root_route;
+mod standard_header;
 
 use axum::{
     body::Body,
     routing::{get, post},
     Router,
 };
-use header_user_agent::header_user_agent;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 use path_variables::path_variables;
 use query_params::query_params;
+use standard_header::standard_header;
 
 pub fn create_routes() -> Router<Body> {
     Router::new()
@@ -23,5 +23,5 @@ pub fn create_routes() -> Router<Body> {
         .route("/mirror_body_json", post(mirror_body_json))
         .route("/path_variables/:id", get(path_variables))
         .route("/query_params", get(query_params))
-        .route("/header_user_agent", get(header_user_agent))
+        .route("/standard_header_user_agent", get(standard_header))
 }
