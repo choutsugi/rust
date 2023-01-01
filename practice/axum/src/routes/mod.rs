597114cc198eee39a03a57111ed7615083b2005b
張継 <choutsugi@gmail.com>
2023-01-01 14:23:14 +0800

    CORS

--- practice/axum/src/routes/mod.rs
+++ practice/axum/src/routes/mod.rs
@@ -8,6 +8,7 @@ mod standard_header;
 
 use axum::{
     body::Body,
+    http::Method,
     routing::{get, post},
     Router,
 };
@@ -17,8 +18,13 @@ use mirror_body_string::mirror_body_string;
 use path_variables::path_variables;
 use query_params::query_params;
 use standard_header::standard_header;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes() -> Router<Body> {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(root_route::hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
@@ -27,4 +33,5 @@ pub fn create_routes() -> Router<Body> {
         .route("/query_params", get(query_params))
         .route("/standard_header_user_agent", get(standard_header))
         .route("/custom_header", get(custom_header))
+        .layer(cors)
 }
