a719e4fbf7653961f013f7aed5a6448e947376f9
張継 <choutsugi@gmail.com>
2023-01-01 13:17:56 +0800

    Path

--- practice/axum/src/routes/mod.rs
+++ practice/axum/src/routes/mod.rs
@@ -1,5 +1,6 @@
 mod mirror_body_json;
 mod mirror_body_string;
+mod path_variables;
 
 mod root_route;
 
@@ -11,10 +12,12 @@ use axum::{
 
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
+use path_variables::path_variables;
 
 pub fn create_routes() -> Router<Body> {
     Router::new()
         .route("/", get(root_route::hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
+        .route("/path_variables/:id", get(path_variables))
 }
