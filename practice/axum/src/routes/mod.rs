192e038036e5a91a303153a9bdfb91b667ff0aa5
張継 <choutsugi@gmail.com>
2023-01-01 13:35:03 +0800

    Query

--- practice/axum/src/routes/mod.rs
+++ practice/axum/src/routes/mod.rs
@@ -1,6 +1,7 @@
 mod mirror_body_json;
 mod mirror_body_string;
 mod path_variables;
+mod query_params;
 
 mod root_route;
 
@@ -13,6 +14,7 @@ use axum::{
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
 use path_variables::path_variables;
+use query_params::query_params;
 
 pub fn create_routes() -> Router<Body> {
     Router::new()
@@ -20,4 +22,5 @@ pub fn create_routes() -> Router<Body> {
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
         .route("/path_variables/:id", get(path_variables))
+        .route("/query_params", get(query_params))
 }
